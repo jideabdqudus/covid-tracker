b6680f2471accf850a185fbeadb67369af79c2e5
jideabdqudus <jideabdqudus@gmail.com>
2020-05-03 15:42:41 +0100

    Api Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import "./App.module.css";
+import styles from "./App.module.css";
 import { Cards, Chart, CountryPicker } from "./components";
+import {fetchData} from "./api"
 
 class App extends Component {
+ async  componentDidMount(){
+    const data = await fetchData()
+
+    console.log(data)
+  }
   render() {
     return (
-      <div>
+      <div className={styles.container}>
         <Cards />
         <CountryPicker />
         <Chart />
