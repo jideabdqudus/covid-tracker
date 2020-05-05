ffe57ca0005e30006cad936fa8d92d3d34787669
jideabdqudus <jideabdqudus@gmail.com>
2020-05-05 13:32:24 +0100

    Daily API

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,22 +1,26 @@
-import React, {useState, useEffect} from 'react'
-import {fetchDailyData, fetchData} from "../../api/index"
+import React, { useState, useEffect } from "react";
+import { fetchDailyData, fetchData } from "../../api/index";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-    const [dailyData, setDailyData] = useState({})
+  const [dailyData, setDailyData] = useState({});
 
-    useEffect(()=>{
-        const fetchAPI = async ()=>{
-            setDailyData(await fetchDailyData())
-        }
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
 
-        fetchAPI()
-    })
-    
-    return (
-        <div>
-        
-        </div>
-    )
-}
+    console.log(dailyData);
 
-export default Chart
+    fetchAPI();
+  });
+
+  const lineChart = dailyData[0] ? (
+    <Line data={{ labels: "", datasets: [{}, {}] }} />
+  ) : null;
+
+  return <div></div>;
+};
+
+export default Chart;
