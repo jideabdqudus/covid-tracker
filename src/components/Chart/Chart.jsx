8022e1d9f089b0ea00c24b1ed0a8f93de2bd65d9
jideabdqudus <jideabdqudus@gmail.com>
2020-05-05 13:54:02 +0100

    Daily API

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from "react-chartjs-2";
 import styles from "./Chart.module.css";
 
 const Chart = () => {
-  const [dailyData, setDailyData] = useState({});
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchAPI = async () => {
@@ -16,19 +16,19 @@ const Chart = () => {
     fetchAPI();
   });
 
-  const lineChart = dailyData[0] ? (
+  const lineChart = (dailyData.length ? (
     <Line
       data={{
-        labels: dailyData(({ date }) => date),
+        labels: dailyData.map(({ date }) => date),
         datasets: [
           {
-            data: dailyData(({ confirmed }) => confirmed),
+            data: dailyData.map(({ confirmed }) => confirmed),
             label: "Infected",
             borderColor: "#3333ff",
             fill: true,
           },
           {
-            data: dailyData(({ deaths }) => deaths),
+            data: dailyData.map(({ deaths }) => deaths),
             label: "Deaths",
             borderColor: "black",
             backgroundColor: "rgba(255,0,0, 0.5)",
@@ -36,10 +36,10 @@ const Chart = () => {
           },
         ],
       }}
-    />
-  ) : null;
+    />) : null
+  ) ;
   return (<div className="styles.container">
-    
+    {lineChart}
     </div>);
 };
 
