d447c1e8a9b6df738f84faa3a2315fd6b0502f70
jideabdqudus <jideabdqudus@gmail.com>
2020-05-06 17:42:15 +0100

    Viruins Edit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ class App extends Component {
 
   state={
     data:{},
+    country: ''
   }
 
  async  componentDidMount(){
     const fetchedData = await fetchData()
 
     this.setState ({data: fetchedData})
+  }
+
+  handleCountryChange =async (country)=>{
+    console.log(country)
+    //fetch the data
+    //set the state
   }
   render() {
     const {data} = this.state
