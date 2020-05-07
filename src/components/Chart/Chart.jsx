8808790d41502db41dda6b05bbc21a8d3f7ee0d4
jideabdqudus <jideabdqudus@gmail.com>
2020-05-07 17:55:52 +0100

    Chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -46,9 +46,9 @@ const Chart = ({ data:{confirmed, deaths, recovered}, country }) => {
         datasets:[{
           label:"People",
           backgroundColor:[
-            "rgba (0,0,255,0.5)",
-            "rgba (0,255,0,0.5)",
-            "rgba (255,0,0,0.5)"
+            "rgba(0,0,255,0.5)",
+            "rgba(0,255,0,0.5)",
+            "rgba(255,0,0,0.5)",
           ],
           data:[confirmed.value, recovered.value, deaths.value]
         }]
