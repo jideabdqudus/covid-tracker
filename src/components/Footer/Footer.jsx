ce11b8fa9198226ee9530ec517e283d49aa9d4e3
jideabdqudus <jideabdqudus@gmail.com>
2020-05-09 21:35:00 +0100

    Added Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import twitter from "./../Viruins/images/twitter.svg";
-import github from "./../Viruins/images/github.svg";
 
 const Footer = () => {
   return (
-    <div>
-        <hr style={{ margin:"30px"}}/>
+    <div className={styles.container}>
+        <hr style={{ margin:"30px"}} />
         <nav className={styles.footerNav}>
         <ul>
-            <li>About</li>
-            <li>Contact</li>
+            <li><a href="https://twitter.com/jideabdqudus">Developer</a></li>
+            <li className={styles.container} style={{right:"0px", position:"absolute"}}><a href="https://github.com/jideabdqudus/covid-tracker">Github</a></li>
         </ul>
         </nav>
     </div>
@@ -19,37 +17,3 @@ const Footer = () => {
 
 export default Footer;
 
-// <div class={styles.container}>
-//           <nav class={styles.footerNav}>
-//             <ul>
-//               <li>
-//                 <a href="https://abdulqudus.com">About Developer</a>
-//               </li>
-//               <li>
-//                 <a href="mailto:jideabdqudus@gmail.com">Contact</a>
-//               </li>
-//             </ul>
-//           </nav>
-//           <nav class={styles.footerNav}>
-//             <ul>
-//               <li>
-//                 <a
-//                   href="https://twiiter.com/jideabdqudus"
-//                   class={styles.socialLink}
-//                 >
-//                   <img src={twitter} />
-//                   Twitter
-//                 </a>
-//               </li>
-//               <li>
-//                 <a
-//                   href="https://github.com/jideabdqudus/covid-tracker"
-//                   class={styles.socialLink}
-//                 >
-//                   <img src={github} />
-//                   Github Repo
-//                 </a>
-//               </li>
-//             </ul>
-//           </nav>
-//         </div>
