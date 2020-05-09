f63266becb36eaa5703e548b0bf5644316632eb5
jideabdqudus <jideabdqudus@gmail.com>
2020-05-09 21:47:06 +0100

    Added Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-        <hr style={{ margin:"30px"}} />
+        <hr style={{ margin:"30px", borderWidth:"1px"}} />
         <nav className={styles.footerNav}>
         <ul>
-            <li><a href="https://twitter.com/jideabdqudus">Developer</a></li>
-            <li className={styles.container} style={{right:"0px", position:"absolute"}}><a href="https://github.com/jideabdqudus/covid-tracker">Github</a></li>
+            <li><a href="https://abdulqudus.com">Developer</a></li>
+            <li ><a href="https://github.com/jideabdqudus/covid-tracker">Github</a></li>
         </ul>
         </nav>
     </div>
