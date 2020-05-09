a89b9234cceccac781fe254ef1935140f2ba4f0a
jideabdqudus <jideabdqudus@gmail.com>
2020-05-09 21:19:16 +0100

    Added Changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import React, { Component } from "react";
 import styles from "./App.module.css";
 import { Cards, Chart, CountryPicker } from "./components";
-import {fetchData} from "./api"
-import Viruins from "./components/Viruins/Viruins"
+import { fetchData } from "./api";
+import Viruins from "./components/Viruins/Viruins";
 import Footer from "./components/Footer/Footer";
 
 class App extends Component {
+  state = {
+    data: {},
+    country: "",
+  };
 
-  state={
-    data:{},
-    country: ''
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
 
- async  componentDidMount(){
-    const fetchedData = await fetchData()
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
 
-    this.setState ({data: fetchedData})
-  }
-
-  handleCountryChange =async (country)=>{
-    const fetchedData = await fetchData(country)
-
-    this.setState({data: fetchedData, country: country})
-
-  }
+    this.setState({ data: fetchedData, country: country });
+  };
   render() {
-    const {data, country} = this.state
+    const { data, country } = this.state;
     return (
       <div>
-      <Viruins/>
-      {/*<div className={styles.container}>
-        <Cards data={data}/>
-        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart data={data} country={country}/>
+        <Viruins />
+        {/*<div className={styles.container}>
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
     </div>*/}
-      
-    <Footer/>
+        <Footer />
       </div>
     );
   }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,44 +6,50 @@ import github from "./../Viruins/images/github.svg";
 const Footer = () => {
   return (
     <div>
-      <footer class={styles.mainFooter}>
-        <div class={styles.container}>
-          <nav class={styles.footerNav}>
-            <ul>
-              <li>
-                <a href="https://abdulqudus.com">About Developer</a>
-              </li>
-              <li>
-                <a href="mailto:jideabdqudus@gmail.com">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <nav class={styles.footerNav}>
-            <ul>
-              <li>
-                <a
-                  href="https://twiiter.com/jideabdqudus"
-                  class={styles.socialLink}
-                >
-                  <img src={twitter} />
-                  Twitter
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/jideabdqudus/covid-tracker"
-                  class={styles.socialLink}
-                >
-                  <img src={github} />
-                  Github Repo
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </footer>
+        <hr style={{ margin:"30px"}}/>
+        <nav className={styles.footerNav}>
+        <ul>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </nav>
     </div>
   );
 };
 
 export default Footer;
+
+// <div class={styles.container}>
+//           <nav class={styles.footerNav}>
+//             <ul>
+//               <li>
+//                 <a href="https://abdulqudus.com">About Developer</a>
+//               </li>
+//               <li>
+//                 <a href="mailto:jideabdqudus@gmail.com">Contact</a>
+//               </li>
+//             </ul>
+//           </nav>
+//           <nav class={styles.footerNav}>
+//             <ul>
+//               <li>
+//                 <a
+//                   href="https://twiiter.com/jideabdqudus"
+//                   class={styles.socialLink}
+//                 >
+//                   <img src={twitter} />
+//                   Twitter
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href="https://github.com/jideabdqudus/covid-tracker"
+//                   class={styles.socialLink}
+//                 >
+//                   <img src={github} />
+//                   Github Repo
+//                 </a>
+//               </li>
+//             </ul>
+//           </nav>
+//         </div>
