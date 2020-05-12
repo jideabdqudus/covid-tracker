66fe0c2d8f632354fd316acde744f407f4d5b7e0
jideabdqudus <jideabdqudus@gmail.com>
2020-05-12 14:18:03 +0100

    Added Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
         <hr style={{ margin:"30px", borderWidth:"1px"}} />
         <nav className={styles.footerNav}>
         <ul>
-            <li><a target="_blank" href="https://abdulqudus.com">Developer</a></li>
-            <li ><a target="_blank" href="https://github.com/jideabdqudus/covid-tracker">Github</a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://abdulqudus.com">Developer</a></li>
+            <li ><a target="_blank" rel="noopener noreferrer" href="https://github.com/jideabdqudus/covid-tracker">Github</a></li>
         </ul>
         </nav>
     </div>
